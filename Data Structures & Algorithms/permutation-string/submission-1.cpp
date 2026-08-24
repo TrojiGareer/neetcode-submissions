aea80b04edb473f5100aeba7f119dc4af8a75a66
Ianis Nicolau <33779075+TrojiGareer@users.noreply.github.com>
2026-08-24 15:19:10 +0300

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int l = s1.length();
+        int n = s2.length();
+        if (l > n) return false;
+        vector<int> freqS1(26, 0);
+        vector<int> freqWindow(26, 0);
+        for (int i = 0; i < l; i++) {
+            freqS1[s1[i] - 'a']++;
+            freqWindow[s2[i] - 'a']++;
+        }
+        for (int i = l; i < n; i++) {
+            int ok = 1;
+            for (int j = 0; j < l && ok; j++) {
+                if (freqS1[s1[j] - 'a'] != freqWindow[s1[j] - 'a']) ok = 0;
+            }
+            if (ok) return true;
+            freqWindow[s2[i - l] - 'a']--;
+            freqWindow[s2[i] - 'a']++;
+        }
+        if (freqS1 == freqWindow) return true;
+        return false;
+    }
+};
